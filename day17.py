7d52781425642f6db22ed5e597bd02f8c7acc51b
Obyduzzaman Abir <obyduzzamanabir@gmail.com>
2026-02-20 03:48:32 +0600

    prblm solving day 18

---
+++ day17.py
@@ -0,0 +1,12 @@
+#basic for loop with conditions
+for i in range (1,11):
+    if i==3 :
+        print("Add this song to fav")
+    else:
+        print(i)
+
+print("2nd loop with condition example ")
+#basic 2 loop with condition
+for i in range(1,101):
+    if i%8==0 and i% 12 ==0:
+        print(i)
