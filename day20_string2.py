4e3ab49973c5fd5dbf208bb30d66710f6ed66cfc
Obyduzzaman Abir <obyduzzamanabir@gmail.com>
2026-02-26 03:48:19 +0600

    End the string function 2

---
+++ day20_string2.py
@@ -0,0 +1,28 @@
+from idlelib.multicall import MC_ENTER
+
+a="hello"
+b="Hello123"
+c="1234"
+d="HELLO"
+e=" "
+f="Hello 123"
+g="1.234"
+#Examine is alphanumaric or not
+print(a,a.isalnum())
+print(b,b.isalnum())
+print(c,c.isalnum())
+print(d,d.isalnum())
+print(e, e.isalnum())
+print(f, f.isalnum())
+print(g, g.isalnum())
+
+print("Examine is alpha or not")
+print("***********")
+#examine alpha
+print(a, a.isalpha())
+print(b, b.isalpha())
+print(c, c.isalpha())
+print(d, d.isalpha())
+
+
+#More functions "isdecimal , isdigit "
